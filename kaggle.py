6cf6d2fe552854ecf8b364864ae2dba76cfaf78b
Hayati <hayati.ibis@yandex.com>
2017-10-31 14:37:49 +0300

    initial commit

---
+++ kaggle.py
@@ -0,0 +1,100 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+import numpy as np
+import os
+
+corpus = []
+
+pos_prob_vector = []
+neg_prob_vector = []
+
+positiveCount = 0
+negativeCount = 0
+
+for file in os.listdir("MRDataset/train/pos"):
+    f = open("MRDataset/train/pos/" + file, encoding='utf-8')
+    context = f.read()
+    corpus.append(context)
+    positiveCount += 1
+
+for file in os.listdir("MRDataset/train/neg"):
+    f = open("MRDataset/train/neg/" + file, encoding='utf-8')
+    context = f.read()
+    corpus.append(context)
+    negativeCount += 1
+
+count_vect = CountVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0.007, max_df=0.4)
+transformer = TfidfTransformer();
+
+train_V = count_vect.fit_transform(corpus)
+train_tf = transformer.fit_transform(train_V)
+
+features = count_vect.get_feature_names()
+# print(list)
+print(len(features))
+
+print("pos" + str(positiveCount))
+print("neg" + str(negativeCount))
+
+# print(train_V)
+# print(train_tf)
+
+train_tf = train_tf.toarray()
+
+n_word_pos = np.sum(train_tf[:positiveCount], axis=0)
+n_word_neg = np.sum(train_tf[positiveCount:], axis=0)
+
+total_word_pos = np.sum(n_word_pos)
+total_word_neg = np.sum(n_word_neg)
+
+pos_prob_vector = ((n_word_pos + 1) / (total_word_pos + len(features)))
+neg_prob_vector = ((n_word_neg + 1) / (total_word_neg + len(features)))
+
+print(neg_prob_vector.shape)
+print(pos_prob_vector.shape)
+
+# Test
+test = []
+testFileNames = []
+testLabels = []
+
+for file in os.listdir("KDataset/"):
+    f = open("KDataset/" + file, encoding='utf-8')
+    context = f.read()
+    testFileNames.append(file)
+    test.append(context)
+
+
+
+test = count_vect.transform(test)
+
+
+def naiveBayes(pos_prob_vector, neg_prob_vector, test):
+    estimatedLabels = []
+    for item in test:
+        test_V = item.toarray()
+        posProb = np.dot(test_V, pos_prob_vector)
+        negProb = np.dot(test_V, neg_prob_vector)
+        if posProb > negProb:
+            estimatedLabels.append("pos")
+        else:
+            estimatedLabels.append("neg")
+
+    return estimatedLabels
+
+
+LabelResult = naiveBayes(pos_prob_vector, neg_prob_vector, test)
+
+
+output = open("kaggle.txt", "w")
+
+output.write("InstanceName,Class\n")
+
+for i in range(len(LabelResult)):
+    output.write(str(testFileNames[i]) + ',' + str(LabelResult[i]) + '\n')
+
+output.close()
+
+
+
+
---
+++ nbc.py
@@ -0,0 +1,117 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+import numpy as np
+import os
+
+corpus = []
+
+pos_prob_vector = []
+neg_prob_vector = []
+
+positiveCount = 0
+negativeCount = 0
+
+for file in os.listdir("MRDataset/train/pos"):
+    f = open("MRDataset/train/pos/" + file, encoding='utf-8')
+    context = f.read()
+    corpus.append(context)
+    positiveCount += 1
+
+for file in os.listdir("MRDataset/train/neg"):
+    f = open("MRDataset/train/neg/" + file, encoding='utf-8')
+    context = f.read()
+    corpus.append(context)
+    negativeCount += 1
+
+count_vect = CountVectorizer(analyzer='word', ngram_range=(1, 2), min_df=0.0005, max_df=0.4)
+transformer = TfidfTransformer();
+
+train_V = count_vect.fit_transform(corpus)
+train_tf = transformer.fit_transform(train_V)
+
+features = count_vect.get_feature_names()
+# print(list)
+print(len(features))
+
+print("pos" + str(positiveCount))
+print("neg" + str(negativeCount))
+
+# print(train_V)
+# print(train_tf)
+
+train_V = train_V.toarray()
+
+n_word_pos = np.sum(train_V[:positiveCount], axis=0)
+n_word_neg = np.sum(train_V[positiveCount:], axis=0)
+
+total_word_pos = np.sum(n_word_pos)
+total_word_neg = np.sum(n_word_neg)
+
+pos_prob_vector = np.log1p((n_word_pos + 1) / (total_word_pos + len(features)))
+neg_prob_vector = np.log1p((n_word_neg + 1) / (total_word_neg + len(features)))
+
+print(neg_prob_vector.shape)
+print(pos_prob_vector.shape)
+
+# Test
+test = []
+testFileNames = []
+testLabels = []
+
+for file in os.listdir("MRDataset/test/pos"):
+    f = open("MRDataset/test/pos/" + file, encoding='utf-8')
+    context = f.read()
+    testFileNames.append(file)
+    test.append(context)
+    testLabels.append("pos")
+
+for file in os.listdir("MRDataset/test/neg"):
+    f = open("MRDataset/test/neg/" + file, encoding='utf-8')
+    context = f.read()
+    testFileNames.append(file)
+    test.append(context)
+    testLabels.append("neg")
+
+test = count_vect.transform(test)
+
+
+def naiveBayes(pos_prob_vector, neg_prob_vector, test):
+    estimatedLabels = []
+    for item in test:
+        test_V = item.toarray()
+        posProb = np.dot(test_V, pos_prob_vector)
+        negProb = np.dot(test_V, neg_prob_vector)
+        if posProb > negProb:
+            estimatedLabels.append("pos")
+        else:
+            estimatedLabels.append("neg")
+
+    return estimatedLabels
+
+
+LabelResult = naiveBayes(pos_prob_vector, neg_prob_vector, test)
+
+
+def accuracy(EstimatedLabels, TrueLabels):
+    count = 0
+    for i in range(len(EstimatedLabels)):
+        if EstimatedLabels[i] == TrueLabels[i]:
+            count += 1
+    return (count / len(EstimatedLabels)) * 100
+
+
+acc = accuracy(LabelResult, testLabels)
+
+output = open("result.txt", "w")
+
+output.write('ACCURACY :' + str(acc) + '\n')
+
+for i in range(len(LabelResult)):
+    output.write(
+        str(testFileNames[i]) + ' TrueLabel :' + str(testLabels[i]) + ' EstimatedLabel :' + str(LabelResult[i]) + '\n')
+
+output.close()
+
+
+
+
